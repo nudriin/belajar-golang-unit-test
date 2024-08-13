6405d14842d057cd049c76af16bd7873d1ec6bf7
Nurdin <mrsunnysummer@gmail.com>
2024-08-13 13:09:58 +0700

    Chapter 2 assertion

--- ch_2_assertion/assertion_test.go
+++ ch_2_assertion/assertion_test.go
@@ -1,13 +1,24 @@
 package ch_2_assertion
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/nudriin/belajar-golang-unit-test/ch_1_helper"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func TestSayHello(t *testing.T) {
+func TestSayHelloAssert(t *testing.T) {
 	result := ch_1_helper.SayHello("Nurdin")
-	assert.Equal(t, "Hello Nurdin", result) // (t, expected, actual)
+	assert.Equal(t, "Hello Nurdins", result) // (t, expected, actual)
+	assert.NotEqual(t, "", result)           // (t, expected, actual)
+	fmt.Println("di print ke sini")
+}
+
+func TestSayHelloRequire(t *testing.T) {
+	result := ch_1_helper.SayHello("Nurdin")
+	require.Equal(t, "Hello Nurdins", result) // (t, expected, actual)
+	require.NotEqual(t, "", result)           // (t, expected, actual)
+	fmt.Println("tidak di print ke sini")
 }
